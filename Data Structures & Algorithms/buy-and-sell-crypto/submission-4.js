d5ea8751c8417f9c7a65dfc2188daa59ebbbacdc
Priyanka <103273242+priyankaj04@users.noreply.github.com>
2026-05-28 14:15:18 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+    let minPrice = Infinity;
+    let maxProfit = 0;
+
+    for (const price of prices) {
+        minPrice = Math.min(minPrice, price);
+        maxProfit = Math.max(maxProfit, price - minPrice);
+    }
+
+    return maxProfit;
+    }
+}
